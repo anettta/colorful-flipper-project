714d51effee4c5d0064a00c86aef94c367c0a0bf
Anya <annagapyuk@gmail.com>
2020-06-11 21:58:52 -0400

    added hex

--- app.js
+++ app.js
@@ -6,12 +6,13 @@ const color = document.querySelector(".color");
 
 btn.addEventListener("click", function () {
     // get random number between 0 - 3 colors
-    const randomNumber = 2;
+    const randomNumber = getRandomNumber();
+
     document.body.style.backgroundColor = colors[randomNumber];
     color.textContent = colors[randomNumber];
 
 });
 
 function getRandomNumber() {
-    return Math.random();
+    return Math.floor(Math.random() * colors.length);
 }
